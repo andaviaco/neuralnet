73391a1f5821738b2f349610647e00b297680382
Andrés Ávila <andaviaco@gmail.com>
2018-02-06 23:42:00 -0600

    remove placeholder code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Perceptrón</h1>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Main from './components/Main.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Main,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h2>Component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+};
+</script>
+
+<style>
+
+</style>
