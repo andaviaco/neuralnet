b40e96b51f856e56daeabd5b9c6683b42aa905ec
Andrés Ávila <andaviaco@gmail.com>
2018-05-01 16:47:09 -0500

    create RBFNetwork integrating RBFLayer and OutputLayer

---
+++ src/models/RBFNetwork/RBFNetwork.js
@@ -0,0 +1,40 @@
+import { zip } from '@/lib';
+import RBFLayer from './RBFLayer';
+import OutputLayer from './OutputLayer';
+
+
+class RBFNetwork {
+  hiddenLayer = null;
+  outputLayer = null;
+  kNearest = 2
+
+  constructor(numInputs, numHiddenNeurons, numOutputNeurones) {
+    this.hiddenLayer = new RBFLayer(numHiddenNeurons, numInputs);
+    this.outputLayer = new OutputLayer(numOutputNeurones, numHiddenNeurons);
+  }
+
+  startTraining(trainingSet, learningRate, maxEpoch, desiredError) {
+    const patterns = trainingSet.map(tset => tset[0]);
+    const expected = trainingSet.map(tset => tset[1]);
+
+    this.hiddenLayer.clustering(patterns);
+
+    const hiddenLayerOutput = this.feedHiddenLayer(patterns);
+    const outputTrainingSet = zip(hiddenLayerOutput, expected);
+
+    this.outputLayer.train(outputTrainingSet, learningRate, maxEpoch, desiredError);
+  }
+
+  feedHiddenLayer(patterns) {
+    return patterns.map(p => this.hiddenLayer.feedForward(p));
+  }
+
+  predict(input) {
+    const hiddenLayerOutput = this.hiddenLayer.feedForward(input);
+    const output = this.outputLayer.feedForward(hiddenLayerOutput);
+
+    return output;
+  }
+}
+
+export default RBFNetwork;
--- src/models/index.js
+++ src/models/index.js
@@ -3,3 +3,4 @@ export { default as Neurone } from './Neurone';
 export { default as Perceptron } from './Perceptron';
 export { default as Adaline } from './Adaline';
 export { default as MultiLayerNetwork } from './MultiLayerNetwork/MultiLayerNetwork';
+export { default as RBFNetwork } from './RBFNetwork/RBFNetwork';
